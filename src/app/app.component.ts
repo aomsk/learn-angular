47ab152576062688f1d1fa8730c2fe32ff1a969a
Sipang K <plaaom39@gmail.com>
2023-06-15 15:37:08 +0700

    chore: add max-min-meter.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,20 +15,28 @@ export class AppComponent {
   // cahngeNinjaName(name: string) {
   //   this.ninjaName = name;
   // }
-
   // 2 ------------------------------------------------------------------------------------
   // value = 0;
   // calBuffet(value: string) {
   //   const price = Number(value);
   //   this.value = (price * 3) / 4;
   // }
-
   // 3 -------------------------------------------------------------------------------------
-  testClick() {
-    console.log('test event binding');
+  // testClick() {
+  //   console.log('test event binding');
+  // }
+  // testNumberChange(value: number) {
+  //   console.log('test number cahnge from app-action-bar:', value);
+  // }
+  // 4 -------------------------------------------------------------------------------------
+  appMinLabel: string = 'myAppMinLabel';
+  appMaxLabel: string = 'myAppMaxLabel';
+
+  doAppMinChange(value: number) {
+    console.log(`test minChange event : ${value}`);
   }
 
-  testNumberChange(value: number) {
-    console.log('test number cahnge from app-action-bar:', value);
+  doAppMaxChange(value: number) {
+    console.log(`test maxChange event : ${value}`);
   }
 }
